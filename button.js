74299157df75d9401922cce8fd9f3a034a1f49f6
Tess Larcade <tesslarcade@gmail.com>
2018-01-17 17:14:35 -0700

    added week1 team activity

---
+++ button.js
@@ -0,0 +1,7 @@
+function bclicked() {
+	alert("Clicked!")
+}
+function ccolor(){
+	var color = document.getElementById('newColor').value;
+	document.getElementById('colorChange').style.backgroundColor = color;
+}
