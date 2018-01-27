36c889a9db18b9d91dda00c9803ece87f1a9c763
Tess Larcade <tesslarcade@gmail.com>
2018-01-27 15:40:42 -0700

    added files

--- web/finish.php
+++ web/finish.php
@@ -1,56 +1,59 @@
-  <!DOCTYPE html>
-  <html>
-  <head>
-  	<link rel="stylesheet" type="text/css" href="css/main.css">
-  	<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Slabo+27px" rel="stylesheet">
-  	<title>Check Out</title>
-  </head>
-  <body>
-  	<header>
-  		<div class='headcont'>
-  			<div class='header'>
-  				<h1>Tess Larcade</h1>
-  			</div>
-  		</div>
-  		<nav>
-  			<ul>
-  				<li>
-  					<a href='index.html'>Home</a>
-  				</li>
-  				<li>
-  					<a href='links.html'>Links</a>
-  				</li>
-  			</ul>
-  		</nav>
-  	</header>
-  	<main>
-      <h2>Order Confirmation</h2>
-      <div>
-        <?php 
-        $info = $_POST['fName'];
-        $info .= "\r\n" . $_POST['lName']; 
-        $info .= "\r\n" . $_POST['address'];
-        $info .= "\r\n" . $_POST['city'];
-        $info .= "\r\n" . $_POST['state']; 
-        $info .= "\r\n" . $_POST['zip']; 
-        $info_clean = htmlspecialchars($info);
-        echo $info_clean;
-
-        $items = $_SESSION['items'];
-        $count = count($items);
-        for ($i = 0; $i < $count; $i++) {
-          $itemDiv = "<div class = 'list'>";
-          $itemDiv .= "  <div> Item: </div>";
-          $itemDiv .= "  <div>" . $items[$i] . "</div>";
-          $itemDiv .= "  <div><input type='button' value='Delete' onclick='removeItem(" . $items[$i] . ")'></div>";
-          $itemDiv .= "</div>";
-          echo $itemDiv;
-        }
-        ?> 
+   <?php 
+   session_start();
+   ?>
+   <!DOCTYPE html>
+   <html>
+   <head>
+     <link rel="stylesheet" type="text/css" href="css/main.css">
+     <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Slabo+27px" rel="stylesheet">
+     <title>Check Out</title>
+   </head>
+   <body>
+     <header>
+      <div class='headcont'>
+       <div class='header'>
+        <h1>Tess Larcade</h1>
       </div>
-    </main>
-    <footer>
-     &copy; Tess Larcade 2018
-   </footer>
- </body>
- </html>
+    </div>
+    <nav>
+     <ul>
+      <li>
+       <a href='index.html'>Home</a>
+     </li>
+     <li>
+       <a href='links.html'>Links</a>
+     </li>
+   </ul>
+ </nav>
+</header>
+<main>
+  <h2>Order Confirmation</h2>
+  <div>
+    <?php 
+    $info = $_POST['fName'];
+    $info .= "\r\n" . $_POST['lName']; 
+    $info .= "\r\n" . $_POST['address'];
+    $info .= "\r\n" . $_POST['city'];
+    $info .= "\r\n" . $_POST['state']; 
+    $info .= "\r\n" . $_POST['zip']; 
+    $info_clean = htmlspecialchars($info);
+    echo $info_clean;
+
+    $items = $_SESSION['items'];
+    $count = count($items);
+    for ($i = 0; $i < $count; $i++) {
+      $itemDiv = "<div class = 'list'>";
+      $itemDiv .= "  <div> Item: </div>";
+      $itemDiv .= "  <div>" . $items[$i] . "</div>";
+      $itemDiv .= "  <div><input type='button' value='Delete' onclick='removeItem(" . $items[$i] . ")'></div>";
+      $itemDiv .= "</div>";
+      echo $itemDiv;
+    }
+    ?> 
+  </div>
+</main>
+<footer>
+ &copy; Tess Larcade 2018
+</footer>
+</body>
+</html>
