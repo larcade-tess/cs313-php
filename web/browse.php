31fcb4daf0fd0eae011a6a82baf16a591468e9d1
Tess Larcade <tesslarcade@gmail.com>
2018-01-27 14:17:01 -0700

    added files

--- web/browse.php
+++ web/browse.php
@@ -1,45 +1,61 @@
-<!DOCTYPE html>
-<html>
-<head>
- <link rel="stylesheet" type="text/css" href="css/main.css">
- <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Slabo+27px" rel="stylesheet">
- <title>Tess Larcade CS313 Shopping</title>
-</head>
-<body>
- <header>
-  <div class='headcont'>
-   <div class='header'>
-    <h1>Tess Larcade</h1>
-  </div>
-</div>
-<nav>
- <ul>
-  <li>
-   <a href='index.html'>Home</a>
- </li>
- <li>
-   <a href='links.html'>Links</a>
- </li>
- <li>
-  <a href='viewCart.php'>View Cart</a>
-</li>
-</ul>
+    <!DOCTYPE html>
+    <html>
+    <head>
+     <link rel="stylesheet" type="text/css" href="css/main.css">
+     <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Slabo+27px" rel="stylesheet">
+     <title>Tess Larcade CS313</title>
+   </head>
+   <body>
+     <header>
+      <div class='headcont'>
+       <div class='header'>
+        <h1>Tess Larcade</h1>
+      </div>
+    </div>
+    <nav>
+     <ul>
+      <li>
+       <a href='index.html'>Home</a>
+     </li>
+     <li>
+       <a href='links.html'>Links</a>
+     </li>
+     <li>
+      <a href='viewCart.php'>View Cart</a>
+    </li>
+  </ul>
 </nav>
 </header>
 <main>
   <div class='grid'>
-    <?php
-    $count = count($items);
-
-    for ($i = 0; $i < $count; $i++)
-    {
-      $itemDiv = "<div> <img src=''> </div>";
-      $itemDiv .= "<div><input type='button' value='Add' onclick='addItem(" . 124+i . ")'></div>";
-      echo $itemDiv;
-    }
-    ?>
-
+  <div><a href="img/beaded_necklace.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(135)">Add Item</button>
   </div>
+    <div><a href="img/bracelet.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(230)">Add Item</button>
+  </div>
+    <div><a href="img/round_earrings_tur.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(425)">Add Item</button>
+  </div>
+    <div><a href="img/silvertur_ear.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(175)">Add Item</button>
+  </div>
+    <div><a href="img/squash.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(593)">Add Item</button>
+  </div>
+    <div><a href="img/turneck.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(322)">Add Item</button>
+  </div>
+    <div><a href="img/turqbracelet.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(339)">Add Item</button>
+  </div>
+    <div><a href="img/turqsilbrace.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(232)">Add Item</button>
+  </div>
+    <div><a href="img/redbeaded.jpg"><img src="http://via.placeholder.com/150x150"></a>
+    <button class='additem' onclick="addItem(385)">Add Item</button>
+  </div>
+</div>
 </main>
 <footer>
  &copy; Tess Larcade 2018
