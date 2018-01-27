a744ffc1fdf3b92a2210bc3090e5a95de99eb29e
Tess Larcade <tesslarcade@gmail.com>
2018-01-26 22:14:32 -0700

    added files

--- web/cart.php
+++ web/cart.php
@@ -1,8 +1,29 @@
 <?php
 session_start();
 echo "session has been started";
-$item_array = array("item");
-for ($i = 0; $i < count(item_array); ++$i) {
-    echo $item_array[$i];
+
+if (!isset($_SESSION['items'])) {
+	$_SESSION['items'] = array();
 }
+
+if (isset($_POST['item'])) {
+	array_push($_SESSION['items'], $_POST['item']);
+}
+
+$count = count($_SESSION['items']);
+for ($i = 0; $i < $count; ++$i) {
+    echo $_SESSION['items'][$i];
+}
+
+//$item_array = array();
+//for ($i = 0; $i < count(item_array); ++$i) {
+//    echo $item_array[$i];
+//}
 ?>
+
+<!-- foreach($_SESSION['animals'] as $key=>$value)
+    {
+    // and print out the values
+    echo 'The value of $_SESSION['."'".$key."'".'] is '."'".$value."'".' <br />';
+    }
+ -->
