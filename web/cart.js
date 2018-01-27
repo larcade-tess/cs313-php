41f8a2d1e37e1df677ffc3eb832775e5f8c6f650
Tess Larcade <tesslarcade@gmail.com>
2018-01-26 20:39:46 -0700

    added files

---
+++ web/cart.js
@@ -0,0 +1,27 @@
+var items = {
+	'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9
+};
+
+function addItem(item) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				console.log('ajax response', this.responseText);
+			} else {
+				console.log('bad juju');
+			}
+		}
+	};
+	xhttp.open("POST", "localhost/cart.php", true);
+	xhttp.send();
+}
+
---
+++ web/cart.php
@@ -0,0 +1,5 @@
+<?php
+	if ($_POST["item"]) {
+		exit('received' . $_POST["item"]);
+	}
+?>
