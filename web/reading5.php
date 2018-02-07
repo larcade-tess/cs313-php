2342cbc551ea20c72f705f5aced722e8863b37be
Tess Larcade <tesslarcade@gmail.com>
2018-02-07 13:20:51 -0700

    added files and db stuff

--- web/reading5.php
+++ web/reading5.php
@@ -1,7 +1,3 @@
-<?php
-requore_once 'connecttodb.php';
-$db = get_db();
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,17 +9,11 @@ $db = get_db();
 	<main>
 		<div>
 			<h1>Apartments</h1>
-		
+
 		</div>
 
 		<div>
-<!-- 			<?php
-			$statement = $db->query('SELECT first_name FROM contact');
-			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-				{
-					echo '<p>' . $row[first_name]
-				}
-				?> -->
+<h3>Test</h3>
 		</div>
 	</main>
 	<footer>
