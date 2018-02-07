7f7e4be5b22b572f524c7f5922c3124c48e4aaf8
Tess Larcade <tesslarcade@gmail.com>
2018-02-07 13:10:30 -0700

    added files and db stuff

---
+++ web/connecttodb.php
@@ -0,0 +1,32 @@
+<?php
+function connect() {
+  $db = NULL;
+  try {
+    $dbUrl = getenv('DATABASE_URL');
+
+    if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+      $dbUrl = "postgres://postgres:password@127.0.0.1:5432/Scriptures";
+    }
+
+    $dbopts = parse_url($dbUrl);
+    echo "<p>$dbUrl</p>\n\n";
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    echo "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName;</p>\n\n";
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  }
+  catch (PDOException $ex) {
+    echo "Error: $ex</p>";
+    die();
+  }
+  return $db;
+}
+connect();
+?>
--- web/reading5.php
+++ web/reading5.php
@@ -1,36 +1,7 @@
-<!-- <?php
-
-$dbUrl = getenv('DATABASE_URL');
-
-if (empty($dbUrl)) {
- // example localhost configuration URL with postgres username and a database called cs313db
-	$dbUrl = "postgres://postgres:password@127.0.0.1:5432/Scriptures";
-}
-
-$dbopts = parse_url($dbUrl);
-
-
-print "<p>$dbUrl</p>\n\n";
-
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
-
-print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName;</p>\n\n";
-
-try {
-	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch (PDOException $ex) {
-	print "<p>error: $ex->getMessage() </p>\n\n";
-	die();
-}
-
-?> -->
-
+<?php
+requore_once 'connecttodb.php'
+$db = get_db();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -38,30 +9,18 @@ catch (PDOException $ex) {
 </head>
 <body>
 	<div>
-
 		<h1>Apartments</h1>
+	</div>
 
-<!-- 		<?php
-
-		foreach ($db->query('SELECT now()') as $row)
-		{
-			print "<p>$row[0]</p>\n\n";
-		}
-
-		$statement = $db->prepare("SELECT apartment, contact FROM JAPdb");
-		$statement->execute();
-// Go through each result
+	<div>
+		<?php
+		$statement = $db->query('SELECT first_name FROM contact')
 		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-				echo '<p>';
-				echo $row['apartment'] . ' ' . $row['contact'] 
-				echo '</p>';
+				echo '<p>' . $row[first_name]
 			}
-			?> -->
-		</div>
+		?>
+	</div>
 
-	</body>
-	</html>
+</body>
+</html>
