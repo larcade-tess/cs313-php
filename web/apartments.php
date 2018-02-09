f05d1e9852f688bbdf69160c6a8a9f31a00100a4
Tess Larcade <tesslarcade@gmail.com>
2018-02-08 17:59:29 -0700

    added files and db stuff

--- web/apartments.php
+++ web/apartments.php
@@ -38,8 +38,8 @@ $db = connect();
 				{
 					if ($row[rented] == false)
 					{
-						echo '<div class = "aptinfo"> <div class="apartmentimg" id="apartment' . $row[apartment_id] . '">' . '</div>';
-						echo '<a href="apartment'. $row[apartment_id] . '.php"><div class="apartment">' . $row[location] . '</div></a>';
+						echo '<a href="apartment'. $row[apartment_id] . '.php"><div class = "aptinfo"> <div class="apartmentimg" id="apartment' . $row[apartment_id] . '">' . '</div>';
+						echo '<div class="apartment">' . $row[location] . '</div></a>';
 						echo '<div class="location">' . $row[price] . ' Monthly </div> </div>';
 					} 
 				}
