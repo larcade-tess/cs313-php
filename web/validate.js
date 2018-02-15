5854127e042a68219573a4c45838984c8a9bfbee
Tess Larcade <tesslarcade@gmail.com>
2018-02-15 16:06:49 -0700

    Added login validation

--- web/validate.js
+++ web/validate.js
@@ -4,11 +4,13 @@ function validate () {
 }
 
 function validatePassword(){
-var pass1=document.getElementById("password1").value;
-var pass=document.getElementById("password").value;
+	var pass1=document.getElementById("password1").value;
+	var pass=document.getElementById("password").value;
 
-if(pass1!=pass)
-	document.getElementById("password").setCustomValidity("Passwords Don't Match");
-else
-	document.getElementById("password1").setCustomValidity('');
+	if(pass1!=pass) {
+		document.getElementById("password").setCustomValidity("Passwords Don't Match");
+	}
+	else{
+		document.getElementById("password1").setCustomValidity('');
+	}
 }
