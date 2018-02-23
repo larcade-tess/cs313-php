2189bd8c856a35bbbd7a484f9d57f969c73010b8
Tess Larcade <tesslarcade@gmail.com>
2018-02-23 15:06:39 -0700

    Added nav changes

--- web/showing.php
+++ web/showing.php
@@ -32,21 +32,25 @@ $db = connect();
 			</div>
 			<div>
 				<form action='thanks.php' method='post'>
-					<div class='name'>
-						<h4>First Name</h4>
-						<input type='Name' name='fName' required>
-						<br>
-						<h4>Last Name</h4>
-						<input type='Name' name='lName' required>
-						<br>
+					<div class='section'>
+						<div class='name'>
+							<h4>First Name</h4>
+							<input type='Name' name='fName' required>
+						</div>
+						<div class='name'>
+							<h4>Last Name</h4>
+							<input type='Name' name='lName' required>
+						</div>
 					</div>
-					<div class='contact'>
-						<h4>Phone Number</h4>
-						<input phone='Phone' name='phone' required>
-						<br>
-						<h4>Email Address</h4>
-						<input email='Email' name='email' required>
-						<br>
+					<div class='section'>
+						<div class='name'>
+							<h4>Phone Number</h4>
+							<input phone='Phone' name='phone' required>
+						</div>
+						<div class='name'>
+							<h4>Email Address</h4>
+							<input email='Email' name='email' required>
+						</div>
 					</div>
 					<h4>Comments</h4>
 					<textarea rows="4" cols="50" name='comments'></textarea>
