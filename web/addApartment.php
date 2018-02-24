142974c48730abd095fd02a5c954e7f3e25a8b90
Tess Larcade <tesslarcade@gmail.com>
2018-02-24 09:26:49 -0700

    Added nav changes

--- web/addApartment.php
+++ web/addApartment.php
@@ -26,69 +26,87 @@ $db = connect();
 		</nav>
 	</header>
 	<main>
-		<div>
-			<h3>Add Apartment</h3>
-		</div>
-		<div>
-			<?php 
-			if (isset ($_SESSION['username']))
-			{
+		<?php 
+		if (isset ($_SESSION['username']))
+		{
 
-			}
-			else
-			{
-				header("Location: login.php");
-				die(); 
-			}
-			?> 
-			<form action='insertApartment.php' method='post'>
-				<h4>Address</h4>
-				<input type='Address' name='location' required>
-				<br>
-				<h4>Price</h4>
-				<input type='Text' name='price' required>
-				<br>
-				<h4>Bed</h4>
-				<select name='bed' required>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-				</select>
-				<br>
-				<h4>Bath</h4>
-				<select name='bath' required>
-					<option value="1">1</option>
-					<option value="1.5">1.5</option>
-					<option value="1.75">1.75</option>
-					<option value="2">2</option>
-					<option value="2.5">2.5</option>
-					<option value="2.75">2.75</option>
-					<option value="3">3</option>
-					<option value="3.5">3.5</option>
-					<option value="3.75">3.75</option>
-					<option value="4">4</option>
-				</select>
-				<br>
-				<h4>Square Footage</h4>
-				<input type='Text' name='sqft' required>
-				<br>
-				<h4>Apartment is rented</h4>
-				<select name='rented'>
-					<option value="true">Rented</option>
-					<option value="false">Not Rented</option>
-				</select>
-				<br>
-				<h4>Details</h4>
-				<textarea rows="4" cols="50" name="details" required></textarea>
-				<br>
-				<h4>Comments</h4>
-				<textarea rows="4" cols="50" name='comments'></textarea>
-				<br>
-				<input type='submit'>
-			</form>
+		}
+		else
+		{
+			header("Location: login.php");
+			die(); 
+		}
+		?> 
+		<div class = 'indent'>
+			<div>
+				<h3>Add Apartment</h3>
+			</div>
+			<div>
+				<form action='insertApartment.php' method='post'>
+					<div class = 'section'>
+						<div class = 'name'>
+							<h4>Address</h4>
+							<input type='Address' name='location' required>
+						</div>
+						<div class = 'name'>
+							<h4>Price</h4>
+							<input type='Text' name='price' required>
+						</div>
+					</div>
+					<div class = 'section'>
+						<div class = 'name'>
+							<h4>Bed</h4>
+							<select name='bed' required>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+							</select>
+						</div>
+						<div class = 'name'>
+							<h4>Bath</h4>
+							<select name='bath' required>
+								<option value="1">1</option>
+								<option value="1.5">1.5</option>
+								<option value="1.75">1.75</option>
+								<option value="2">2</option>
+								<option value="2.5">2.5</option>
+								<option value="2.75">2.75</option>
+								<option value="3">3</option>
+								<option value="3.5">3.5</option>
+								<option value="3.75">3.75</option>
+								<option value="4">4</option>
+							</select>
+						</div>
+					</div>
+					<div class = 'section'>
+						<div class = 'name'>
+							<h4>Square Footage</h4>
+							<input type='Text' name='sqft' required>
+						</div>
+						<div class = 'name'>
+							<h4>Apartment is rented</h4>
+							<select name='rented'>
+								<option value="true">Rented</option>
+								<option value="false">Not Rented</option>
+							</select>
+						</div>
+					</div>
+					<div class = 'section'>
+						<div class = 'name'>
+							<h4>Details</h4>
+							<textarea rows="4" cols="50" name="details" required></textarea>
+						</div>
+						<div class = 'name'>
+							<h4>Comments</h4>
+							<textarea rows="4" cols="50" name='comments'></textarea>
+						</div>
+					</div>
+					<input type='submit' class = 'button'>
+				</form>
+			</div>
 		</div>
 	</main>
 	<footer>
